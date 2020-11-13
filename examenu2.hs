7d9d950cec45f515dd37940c4e2f7cb78983cda7
SweetAffiGL <71892886+SweetAffiGL@users.noreply.github.com>
2020-11-13 11:26:08 -0600

    Update examenu2.hs

--- examenu2.hs
+++ examenu2.hs
@@ -26,26 +26,26 @@ mes d = do
 fin m = do
     case m of
         1 -> do
-            putStrLn("Naciste en enero, tu mes tiene 31 días")
+            putStrLn("Tu mes enero, tu mes tiene 31 días")
         2 -> do
-            putStrLn("Naciste en febrero, tu mes tiene 28 días")
+            putStrLn("Tu mes febrero, tu mes tiene 28 días")
         3 -> do
-            putStrLn("Naciste en marzo, tu mes tiene 31 días")
+            putStrLn("Tu mes marzo, tu mes tiene 31 días")
         4 -> do
-            putStrLn("Naciste en abril, tu mes tiene 30 días")
+            putStrLn("Tu mes abril, tu mes tiene 30 días")
         5 -> do
-            putStrLn("Naciste en mayo, tu mes tiene 31 días")
+            putStrLn("Tu mes mayo, tu mes tiene 31 días")
         6 -> do
-            putStrLn("Naciste en junio, tu mes tiene 30 días")
+            putStrLn("Tu mes junio, tu mes tiene 30 días")
         7 -> do
-            putStrLn("Naciste en julio, tu mes tiene 31 días")
+            putStrLn("Tu mes julio, tu mes tiene 31 días")
         8 -> do
-            putStrLn("Naciste en agosto, tu mes tiene 31 días")
+            putStrLn("Tu mes agosto, tu mes tiene 31 días")
         9 -> do
-            putStrLn("Naciste en septiembre, tu mes tiene 30 días")
+            putStrLn("Tu mes septiembre, tu mes tiene 30 días")
         10 -> do
-            putStrLn("Naciste en octubre, tu mes tiene 31 días")
+            putStrLn("Tu mes octubre, tu mes tiene 31 días")
         11 -> do
-            putStrLn("Naciste en noviembre, tu mes tiene 30 días")
+            putStrLn("Tu mes noviembre, tu mes tiene 30 días")
         12 -> do
-            putStrLn("Naciste en diciembre, tu mes tiene 31 días")
+            putStrLn("Tu mes diciembre, tu mes tiene 31 días")
